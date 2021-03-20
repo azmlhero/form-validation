4e191c893da95f9e9e5d152b25023e4d1d321063
ammar <azml1@gmail.com>
2021-03-21 00:48:48 +0500

    whole changing

--- myjscode.js
+++ myjscode.js
@@ -1,51 +1,127 @@
 $(function () {
-    $("#myForm").submit(handleFormSubmit);
+  $("#myForm").submit(handleFormSubmit);
+});
+
+function handleFormSubmit(e) {
+  alert("Form is being submitted");
+
+  e.preventDefault();
+}
+
+$(function(){
+  $("#name_error_message").hide();
+  $("#email_error_message").hide();
+  $("#password_error_message").hide();
+  $("#confirm_password_error_message").hide();
+
+  var error_name=false;
+  var error_email=false;
+  var error_password=false;
+
+  var error_confirm_password=false;
+
+  $("#name").focusout(function(){
+      chek_name();
   });
-  $(function() {
-    $("#myform").validate({
-      errorClass: "error fail-alert",
-      validClass: "valid success-alert",
-      rules: {
-        name : {
-          required: true,
-          minlength: 5
-        },
-        
-        email: {
-          required: true,
-          email: true
-        },
-        weight: {
-          required: {
-            depends: function(elem) {
-              return $("#age").val() > 50
-            }
-          },
-          number: true,
-          min: 0
-        }
-      },
-      messages : {
-        name: {
-          minlength: "Name should be at least 3 characters"
-        },
-        age: {
-          required: "Please enter your age",
-          number: "Please enter your age as a numerical value",
-          min: "You must be at least 18 years old"
-        },
-        email: {
-          email: "The email should be in the format: abc@domain.tld"
-        },
-        weight: {
-          required: "People with age over 50 have to enter their weight",
-          number: "Please enter your weight as a numerical value"
-        }
-      }
-    });
-  });
-  function handleFormSubmit(e) {
-    alert("Form is being submitted");
   
-    e.preventDefault();
+  $("#email").focusout(function(){
+      chek_email();
+  });
+  // name shi type karo form nai hay id    aaaaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhh
+  $("#password").focusout(function(){
+      chek_password();
+  });
+  
+  $("#confirm_password").focusout(function(){
+      chek_confirm_password();
+  });
+  
+  function chek_name(){
+
+      var name_length= $("#name").val().length;
+
+      if(name_length<5)
+      {
+          $("#name_error_message").html("Name should contain atleast 5 characters!!!");
+         
+          $("#name_error_message").show();
+         
+          error_name = true;
+      }
+    
+      else
+      {
+          $("name_error_message").hide();
+      }
+
   }
+
+  function chek_password(){
+
+      var password_length= $("#password").val().length;
+
+      if(password_length<8)
+      {
+          $("#password_error_message").html("Password should contain atleast 8 characters!!!");
+         
+          $("#password_error_message").show();
+         
+          error_password = true;
+      }
+    
+      else
+      {
+          $("password_error_message").hide();
+      }
+
+  }
+  
+  
+  function chek_confirm_password(){
+
+      var password= $("#password").val();
+      var confirm_password= $("#confirm_password").val();
+
+      if(password != confirm_password)
+      {
+          $("#confirm_password_error_message").html("Password doesn't match");
+         
+          $("#confirm_password_error_message").show();
+         
+          error_password = true;
+      }
+    
+      else
+      {
+          $("password_error_message").hide();
+      }
+
+  }
+
+  $("#myform").submit(function(){
+      error_name=false;
+      error_password=false;
+      error_confirm_password=false;
+
+      chek_name();
+      chek_password();
+      chek_confirm_password();
+
+      if(error_name==false && error_password== false && error_confirm_password==false)
+      {
+          return true;
+      }
+      else{
+          return false;
+      }
+  });
+
+
+});
+
+
+
+
+
+
+
